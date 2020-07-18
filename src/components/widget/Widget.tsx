cb316d8a1850ee0ce25e26a4ffddb2e78ef03d88
japraj <japraj@live.ca>
2020-07-17 18:48:11 -0700

    reorganize widget wrapper/header/section to be exported from a single file to reduce redundancy

--- src/components/widget/Widget.tsx
+++ src/components/widget/Widget.tsx
@@ -1,6 +1,20 @@
 import styled from "styled-components";
 
-export default styled.div`
+export const WidgetWrapper = styled.section`
+  background-color: var(--dark);
+  color: var(--text-color);
+  border-radius: 8px;
+  height: auto;
+  width: 100%;
+  box-shadow: 0 5px 5px 0 rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 1px 2px var(--shadow-2);
+`;
+
+export const WidgetHeader = styled.div`
   height: auto;
   width: 100%;
   display: flex;
@@ -20,3 +34,5 @@ export default styled.div`
     font-size: 1.25rem;
   }
 `;
+
+export const WidgetSection = styled.div``;
