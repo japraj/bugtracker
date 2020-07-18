f79cda4c54bdd55508d0062b136c9ca6a70d7509
japraj <japraj@live.ca>
2020-07-17 18:38:43 -0700

    make loading animation more dynamic (optional length prop) and convert from css to styled-components

--- src/components/loadingRing/LoadingRing.tsx
+++ src/components/loadingRing/LoadingRing.tsx
@@ -1,11 +1,62 @@
 import React from "react";
-import "./LoadingRing.css";
+import styled, { keyframes } from "styled-components";
 
-export default () => (
-  <div className="lds-ring">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-  </div>
-);
+type Props = { length: number };
+
+export default class extends React.Component<Props, {}> {
+  static defaultProps = {
+    length: 150,
+  };
+
+  render = () => (
+    <RingWrapper length={this.props.length}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </RingWrapper>
+  );
+}
+
+const loadingAnimation = keyframes`
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+`;
+
+const selectLength = (props: Props): number => props.length;
+
+const RingWrapper = styled.div`
+  display: inline-block;
+  position: relative;
+  width: ${selectLength}px;
+  height: ${selectLength}px;
+
+  div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: ${selectLength}px;
+    height: ${selectLength}px;
+    margin: 8px;
+    border: 8px solid #fff;
+    border-radius: 50%;
+    animation: ${loadingAnimation} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: #fff transparent transparent transparent;
+  }
+
+  div: nth-child(1) {
+    animation-delay: -0.45s;
+  }
+
+  div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+
+  div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+`;
