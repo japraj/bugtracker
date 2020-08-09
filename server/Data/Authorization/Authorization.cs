fda1d5d6d7b1c50a839618919dd4df320c4b1b40
japraj <japraj@live.ca>
2020-08-08 17:53:46 -0700

    add basic authorization object

---
+++ server/Data/Authorization/Authorization.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using server.Models.Session;
+using server.Models.User;
+
+namespace server.Data.Authorization
+{
+    public class Authorization
+    {
+        public static bool HasRank
+            (Rank requiredRank, IUserRepo _repository, HttpRequest Request) =>
+            _repository.GetUserBySession(Request.Cookies[Session.KEY]).Rank >= (int)requiredRank;
+
+        // Need an 'IsAuthor' function
+    }
+}
---
+++ server/Data/Authorization/Rank.cs
@@ -0,0 +1,11 @@
+﻿namespace server.Models.User
+{
+    public enum Rank
+    {
+        Blacklisted,
+        User,
+        Developer,
+        Manager,
+        Admin
+    }
+}
