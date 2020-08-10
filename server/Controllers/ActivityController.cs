5be0dbb1fc4db6be36881ed70616575295425227
japraj <japraj@live.ca>
2020-08-09 19:45:06 -0700

    add boilerplate for activity and tickets controllers

---
+++ server/Controllers/ActivityController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using server.Data.ActivityData;
+using server.Models.ActivityModel;
+
+#nullable enable
+
+namespace server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ActivityController : ControllerBase
+    {
+        private readonly IActivityRepo _repository;
+
+        public ActivityController(IActivityRepo repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{id}", Name = "GetActivityById")]
+        public ActionResult<Activity> GetActivityById(int id)
+        {
+            Activity? activity = _repository.GetActivityById(id);
+            if (activity == null)
+                return NotFound();
+            return Ok(activity);
+        }
+    }
+}
---
+++ server/Controllers/TicketsController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using server.Data.TicketsData;
+using server.Models.TicketModel;
+
+#nullable enable
+
+namespace server.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TicketsController : ControllerBase
+    {
+        private readonly ITicketRepo _repository;
+        private readonly IMapper _mapper;
+
+        public TicketsController(ITicketRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}", Name = "GetTicketById")]
+        public ActionResult<Ticket> GetTicketById(int id)
+        {
+            Ticket? ticket = _repository.GetTicketById(id);
+            if (ticket == null)
+                return NotFound();
+            return Ok(ticket);
+        }
+    }
+}
