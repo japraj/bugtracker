39bb6f3bda99b324735d8e86d96e67072bc2aede
japraj <japraj@live.ca>
2020-08-29 22:14:15 -0700

    fix timestamp to account for timezone offset

--- client/src/components/global/timestamp/index.tsx
+++ client/src/components/global/timestamp/index.tsx
@@ -5,14 +5,29 @@ import { PopperContent } from "../userLink/styles";
 import "../userLink/Popper.css";
 import styled from "styled-components";
 
-const formattedDate = (date: string): string => new Date(date).toLocaleString();
+const getDateFromISO = (dateStr: string): Date => {
+  const d: number[] = dateStr.split(/\D/).map((s) => {
+    try {
+      return Number(s);
+    } catch {
+      return 0;
+    }
+  });
+  var date: Date = new Date(d[0], d[1] - 1, d[2], d[3], d[4], d[5]);
+  date.setTime(date.getTime() - date.getTimezoneOffset() * 60 * 1000);
+  return date;
+};
+
+const formattedDate = (date: string): string =>
+  getDateFromISO(date).toLocaleString();
 
 const shortenDate = (dateStr: string): string => {
   // if the date is within the past 24 hrs, then we want to return a time like xx:yy p.m. (in the user's local time)
   // else, we want to return something of the form "dd/mm/yyyy"
-  const date: Date = new Date(dateStr);
+  const date: Date = getDateFromISO(dateStr);
   return new Date().getTime() - date.getTime() < 24 * 60 * 60 * 1000
-    ? date.toLocaleTimeString()
+    ? date.toLocaleTimeString().substring(0, 4) +
+        date.toLocaleTimeString().substring(7)
     : date.toLocaleDateString();
 };
 
