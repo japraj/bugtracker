3bb3b37f4bd47c957ca944954a0c371983ab6af6
japraj <japraj@live.ca>
2021-02-12 18:37:16 -0800

    Updates

---
+++ precache-manifest.337435457c55e6a3ac60caa7f69b0c31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56b668382cb23d121027ad23d673913c",
+    "url": "/bugtracker/index.html"
+  },
+  {
+    "revision": "3a1da70008c506565ef6",
+    "url": "/bugtracker/static/css/2.efd39d59.chunk.css"
+  },
+  {
+    "revision": "f2021351512c5e078e55",
+    "url": "/bugtracker/static/css/main.ab5955b7.chunk.css"
+  },
+  {
+    "revision": "3a1da70008c506565ef6",
+    "url": "/bugtracker/static/js/2.fb36d9e8.chunk.js"
+  },
+  {
+    "revision": "d5f22024f3313f48040924a4cd45a4ed",
+    "url": "/bugtracker/static/js/2.fb36d9e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2021351512c5e078e55",
+    "url": "/bugtracker/static/js/main.2220520b.chunk.js"
+  },
+  {
+    "revision": "b910e02a8cf2e08859f2",
+    "url": "/bugtracker/static/js/runtime-main.689c6f34.js"
+  },
+  {
+    "revision": "a02e282d673a77945652785baef3ba48",
+    "url": "/bugtracker/static/media/blur-bg.a02e282d.jpg"
+  }
+]);
